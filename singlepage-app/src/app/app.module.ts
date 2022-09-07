03828b37aa13ea685fe7bfdb7f73d5c646f84e6e
Quentin Serda <quentinserda@gmail.com>
2022-09-08 01:12:06 +0200

    set up project, next step - create responsive queries for project

--- singlepage-app/src/app/app.module.ts
+++ singlepage-app/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GridComponent } from './grid/grid/grid.component';
+import {MatGridListModule} from "@angular/material/grid-list";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
