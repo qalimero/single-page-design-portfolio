8c12e4318996a6ffe023714437ba7a983f84d28f
q.serda <quentinserda@gmail.com>
2022-09-19 22:28:09 +0200

    angular

--- singlepage-app/src/app/app.module.ts
+++ singlepage-app/src/app/app.module.ts
@@ -2,25 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GridComponent } from './grid/grid/grid.component';
-import {MatGridListModule} from "@angular/material/grid-list";
-import { IntroductionComponent } from './introduction/introduction/introduction.component';
-import { ParagraphComponent } from './components/atoms/paragraph/paragraph.component';
-import { HeadingLargeComponent } from './components/atoms/heading-large/heading-large.component';
-import { PictureComponent } from './components/atoms/picture/picture.component';
+import { MatGridListModule } from "@angular/material/grid-list";
+import { ImageComponent } from "./components/atoms/image/image.component";
+import { TextComponent } from './components/atoms/text/text.component';
+import { HeaderFooterComponent } from './components/molecules/header-footer/header-footer.component';
 import { ButtonComponent } from './components/atoms/button/button.component';
-import { LogoButtonComponent } from './components/molecules/logo-button/logo-button.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    GridComponent,
-    IntroductionComponent,
-    ParagraphComponent,
-    HeadingLargeComponent,
-    PictureComponent,
+    ImageComponent,
+    TextComponent,
+    HeaderFooterComponent,
     ButtonComponent,
-    LogoButtonComponent
   ],
   imports: [
     BrowserModule,
@@ -30,4 +25,4 @@ import { LogoButtonComponent } from './components/molecules/logo-button/logo-but
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
