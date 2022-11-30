b7366d685b19e9f93b6f9b420925ea9057246ec7
q.serda <quentinserda@gmail.com>
2022-11-30 22:44:48 +0100

    add swiper

--- singlepage-app/src/app/components/my-work-slider/my-work-slider.component.ts
+++ singlepage-app/src/app/components/my-work-slider/my-work-slider.component.ts
@@ -1,9 +1,13 @@
 import {
-  Component, ElementRef,
-  Input, QueryList, ViewChild, ViewChildren,
+  Component, NgModule,
   ViewEncapsulation
 } from '@angular/core';
-import {animate, AnimationBuilder, AnimationFactory, AnimationPlayer, style} from "@angular/animations";
+import { SwiperModule } from 'swiper/angular';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper/core';
+
+@NgModule({
+  imports: [SwiperModule],
+})
 
 @Component({
   selector: 'app-my-work-slider',
@@ -20,81 +24,38 @@ import {animate, AnimationBuilder, AnimationFactory, AnimationPlayer, style} fro
         </ng-container>
       </div>
       <div class="my-work-slider_controls">
-        <app-button id="prev" (click)="prev(index)" classToAdd="btn_slider btn_slider-prev">Prev</app-button>
-        <app-button id="next" (click)="next(index)" classToAdd="btn_slider btn_slider-next">Next</app-button>
+        <app-button id="prev" classToAdd="btn_slider btn_slider-prev">Prev</app-button>
+        <app-button id="next" classToAdd="btn_slider btn_slider-next">Next</app-button>
       </div>
     </section>
   `
 })
 
 export class MyWorkSliderComponent {
-  @Input() timing = '250ms ease-in';
-  counter = 1;
-  @ViewChild('workSlider') workSlider: ElementRef | undefined;
-  slides: any [] = new Array(5).fill({id: -1, src: '', alt: ''});
-  private player: AnimationPlayer | undefined;
-  public index = this.slides.findIndex(slide => slide.src === slide.src);
 
-  constructor(private builder: AnimationBuilder) {
-  }
-
-  ngOnInit() {
-    this.slides = [
-      {
-        src: '../assets/images/image-slide-1.jpg',
-        alt: ""
-      },
-      {
-        src: '../assets/images/image-slide-2.jpg',
-        alt: ""
-      },
-      {
-        src: '../assets/images/image-slide-3.jpg',
-        alt: ""
-      },
-      {
-        src: '../assets/images/image-slide-4.jpg',
-        alt: ""
-      },
-      {
-        src: '../assets/images/image-slide-5.jpg',
-        alt: ""
-      }
-    ]
-  }
-
-  private buildAnimation(translateX: number | undefined) {
-    return this.builder.build([
-      animate(this.timing, style({transform: `translateX(-${translateX}px)`}))
-    ]);
-  }
-
-  prev(index: number) {
-    if (this.index <= 5 && this.index !== 0) {
-      this.index--;
+  slides = [
+    {
+      src: '../assets/images/image-slide-1.jpg',
+      alt: ""
+    },
+    {
+      src: '../assets/images/image-slide-2.jpg',
+      alt: ""
+    },
+    {
+      src: '../assets/images/image-slide-3.jpg',
+      alt: ""
+    },
+    {
+      src: '../assets/images/image-slide-4.jpg',
+      alt: ""
+    },
+    {
+      src: '../assets/images/image-slide-5.jpg',
+      alt: ""
     }
-    //console.log(this.slides[0].elementRef.nativeElement.clientWidth);
-    //console.log(this.workSlider?.nativeElement?.children[0].clientWidth);
-    //console.log(this.workSlider?.nativeElement.clientWidth);
-    console.log(this.index);
-    let wrapperWidth = this.workSlider?.nativeElement.clientWidth;
-    let translateX = wrapperWidth * this.index;
-    console.log(translateX);
-    console.log(wrapperWidth);
-    let myAnimation: AnimationFactory = this.buildAnimation(translateX);
-    this.player = myAnimation.create(this.workSlider?.nativeElement);
-    this.player.play();
-  }
+  ]
 
-  next(index: number) {
-    if (this.index >= 0 && this.index < 5) {
-       this.index++;
-    }
-    let wrapperWidth = this.workSlider?.nativeElement.clientWidth * this.index;
-    console.log(wrapperWidth);
-    console.log(this.index);
-    let myAnimation: AnimationFactory = this.buildAnimation(wrapperWidth);
-    this.player = myAnimation.create(this.workSlider?.nativeElement);
-    this.player.play();
-  }
+  constructor() {}
+
 }
